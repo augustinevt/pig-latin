70b45a094a260d3c22cf738f12fb36b55daea14c
Augustine von Trapp and George Olson <augustine@>
2016-06-08 09:53:51 -0700

    Add function in business logic to move consonant to end of word and add ay if the first letter of the word is not a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
+///business logic
+
+function checkVowel(word) {
+  var vowels = ['a','e','i','o','u'];
+  var match = false;
+  vowels.forEach(function(vowel) {
+    console.log(vowel + "  " + word.charAt(0) )
+    if (word.charAt(0) === vowel) {
+      alert('worked!');
+      match = true;
+    }
+  });
+  return match;
+}
+
+function addAy(word, match) {
+  if (match === false){
+    var firstLetter = word.charAt(0);
+    var restOfWord = word.substring(1);
+    var ayResult = restOfWord.concat(firstLetter + "ay");
+    console.log(ayResult);
+  }
+}
+
+
+///user interface logic
 $(document).ready(function(){
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
 
     var userInput = $("input#input").val();
-    var result = userInput;
+
+    var result = checkVowel(userInput);
+
+    addAy(userInput, result);
+
+    // debugger;
 
     $(".output").text(result);
 
